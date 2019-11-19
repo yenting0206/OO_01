ea93fd3cac6ba17c8384c44a30e0294ec127e62f
yenting0206 <55127491+yenting0206@users.noreply.github.com>
2019-11-19 15:50:13 +0800

    Create HW1.java

---
+++ HW1.java
@@ -0,0 +1,12 @@
+請新增一個CShape的子類別CTriangle，其constructor 共有三個double的參數 a,b,c (為直角三角形的三個邊長)，其面積為0.5*a*b，請寫出CTriangle的類別程式與產生其物件的main 程式(顏色為紅色，a=3, b=4, c=5) 程式碼請放在個人的github 答案請列出其連結
+
+abstract class CShape
+{
+  protected double a,b,c;
+  
+  
+ class CTriangle extends CShape
+ {
+ 
+ }
+
